23d15fb87477556425f0cd80e09a2f4903507072
parth00008 <142876015+parth00008@users.noreply.github.com>
2023-10-07 12:13:51 +0530

    module-4.1(assignment c++)

---
+++ mod 4.1.cpp
@@ -0,0 +1,18 @@
+//
+#include<iostream>
+using namespace std;
+class alphabet{
+	public:
+		void word()
+		{
+		
+			cout<<"------Hello World-------"<<endl;
+		}
+};
+int main()
+{
+	alphabet s1;
+	s1.word();
+}
+
+
